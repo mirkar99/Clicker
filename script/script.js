cef5bd1888211077741307aa6e3d37fa8e87f8e6
Bartosz Kosik <mirkar99@gmail.com>
2022-11-04 17:59:04 +0100

    js added functionality to new DOM  elemenets

--- script/script.js
+++ script/script.js
@@ -5,16 +5,24 @@ const clickBoosters = [...document.querySelectorAll('.section__element')];
 let multiply = 1;
 let autoClickAdder = 0;
 
-const whenBtnClicked = () => clickCounter.innerText = Number(clickCounter.innerText) + 1 * multiply;
 const clickCounterTakeAmount = amount => clickCounter.innerText = Number(clickCounter.innerText) - amount;
 const changeMultiply = a => multiply = a;
 const deleteDomElement = el => el.remove();
+const showHiddenBoosters = (index) => clickBoosters[index + 1].classList.remove('hidden');
+
+const whenBtnClicked = () => clickCounter.innerText = Number(clickCounter.innerText) + 1 * multiply;
+const whenMultiplierBtnClicked = function (amount, el, multiplyBy) {
+    clickCounterTakeAmount(amount);
+    deleteDomElement(el);
+    changeMultiply(multiplyBy);
+}
+
 setInterval(() => { clickCounter.innerText = Number(clickCounter.innerText) + autoClickAdder; }, 1000);
 
 btn.addEventListener('click', () => {
     whenBtnClicked();
 });
-clickBoosters.forEach(el => {
+clickBoosters.forEach((el, index) => {
     el.addEventListener('click', () => {
         if (el.children[1].innerHTML == 'Auto click +1') {
             if (Number(clickCounter.innerHTML) >= Number(el.children[0].innerHTML)) {
@@ -27,9 +35,28 @@ clickBoosters.forEach(el => {
         if (el.children[1].innerHTML == 'Click multiplier x2') {
             if (Number(clickCounter.innerHTML) >= Number(el.children[0].innerHTML)) {
                 let amount = Number(el.children[0].innerHTML);
-                clickCounterTakeAmount(amount);
-                deleteDomElement(el);
-                changeMultiply(2);
+                whenMultiplierBtnClicked(amount, el, 2);
+                showHiddenBoosters(index);
+            }
+        }
+        if (el.children[1].innerHTML == 'Click multiplier x3') {
+            if (Number(clickCounter.innerHTML) >= Number(el.children[0].innerHTML)) {
+                let amount = Number(el.children[0].innerHTML);
+                whenMultiplierBtnClicked(amount, el, 3);
+                showHiddenBoosters(index);
+            }
+        }
+        if (el.children[1].innerHTML == 'Click multiplier x4') {
+            if (Number(clickCounter.innerHTML) >= Number(el.children[0].innerHTML)) {
+                let amount = Number(el.children[0].innerHTML);
+                whenMultiplierBtnClicked(amount, el, 4);
+                showHiddenBoosters(index);
+            }
+        }
+        if (el.children[1].innerHTML == 'Click multiplier x5') {
+            if (Number(clickCounter.innerHTML) >= Number(el.children[0].innerHTML)) {
+                let amount = Number(el.children[0].innerHTML);
+                whenMultiplierBtnClicked(amount, el, 5);
             }
         }
     })
