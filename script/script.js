5684c3155a0caa23f9a150aa63c1653a47be9dfe
Bartosz Kosik <mirkar99@gmail.com>
2022-11-04 20:38:05 +0100

    js added save functionality

--- script/script.js
+++ script/script.js
@@ -15,12 +15,21 @@ const whenBtnClicked = () => clickCounter.innerText = Number(clickCounter.innerT
 
 setInterval(() => { clickCounter.innerText = Number(clickCounter.innerText) + autoClickAdder; }, 1000);
 
+const save = function () {
+    localStorage.setItem('clickValue', clickCounter.innerHTML);
+    localStorage.setItem('multiplyValue', multiply);
+    localStorage.setItem('autoClickAdderValue', autoClickAdder);
+    localStorage.setItem('autoClickBoosterAmount', clickBoosters[0].children[0].innerHTML);
+    localStorage.setItem('multiplyClickBoosterAmount', clickBoosters[1].children[0].innerHTML);
+    localStorage.setItem('multiplyClickBoosterAmount', clickBoosters[1].children[0].innerHTML);
+    localStorage.setItem('multiplyClickBoosterText', clickBoosters[1].children[1].innerHTML);
+}
 btn.addEventListener('click', () => {
     whenBtnClicked();
 });
 clickBoosters.forEach((el, index) => {
     el.addEventListener('click', () => {
-        if (index===0) {
+        if (index === 0) {
             if (Number(clickCounter.innerHTML) >= Number(el.children[0].innerHTML)) {
                 let amount = Number(el.children[0].innerHTML);
                 el.children[0].innerHTML = Number(el.children[0].innerHTML) * 2;
@@ -28,14 +37,17 @@ clickBoosters.forEach((el, index) => {
                 autoClickAdder += 1;
             }
         }
-        if (index===1) {
+        if (index === 1) {
             if (Number(clickCounter.innerHTML) >= Number(el.children[0].innerHTML)) {
                 let amount = Number(el.children[0].innerHTML);
                 clickCounterTakeAmount(amount);
                 changeMultiply();
                 el.children[0].innerHTML = Number(el.children[0].innerHTML) * 2;
-                el.children[1].innerHTML = `Click multiplier x${multiply+1}`;
+                el.children[1].innerHTML = `Click multiplier x${multiply + 1}`;
             }
         }
     })
 });
+saveBtn.addEventListener('click', () => {
+    save();
+})
