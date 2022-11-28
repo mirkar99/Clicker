b5919beee76e5d12bf8fad22ec3bda57351fc557
Bartosz Kosik <mirkar99@gmail.com>
2022-11-28 15:44:24 +0100

    restored functionality  with some changes

--- script/script.js
+++ script/script.js
@@ -1,10 +1,9 @@
 const btn = document.querySelector('.main__button');
 const clickCounter = document.querySelector('.main__text');
 const clickBoosters = [...document.querySelectorAll('.section__element')];
-/*
 const saveBtn = [...document.querySelectorAll('.menu__button')][0];
 const loadBtn = [...document.querySelectorAll('.menu__button')][1];
-*/
+
 
 let multiply = 1;
 let autoClickAdder = 0;
@@ -18,51 +17,53 @@ const whenBtnClicked = () => clickCounter.innerText = Number(clickCounter.innerT
 
 setInterval(() => { clickCounter.innerText = Number(clickCounter.innerText) + autoClickAdder; }, 1000);
 
-/*const save = function () {
+const save = function () {
     localStorage.setItem('SaveWasDone', true);
     localStorage.setItem('clickValue', clickCounter.innerText);
     localStorage.setItem('multiplyValue', multiply);
     localStorage.setItem('autoClickAdderValue', autoClickAdder);
-    localStorage.setItem('autoClickBoosterAmount', clickBoosters[0].children[0].innerHTML);
-    localStorage.setItem('multiplyClickBoosterAmount', clickBoosters[1].children[0].innerHTML);
-    localStorage.setItem('multiplyClickBoosterText', clickBoosters[1].children[1].innerHTML);
+    localStorage.setItem('autoClickBoosterAmount', clickBoosters[0].children[0].innerText);
+    localStorage.setItem('multiplyClickBoosterAmount', clickBoosters[1].children[0].innerText);
+    localStorage.setItem('multiplyClickBoosterText', clickBoosters[1].children[1].innerText);
 }
 const load = function () {
     if(localStorage.getItem('SaveWasDone')){
     clickCounter.innerText = localStorage.getItem('clickValue');
     multiply = Number(localStorage.getItem('multiplyValue'));
     autoClickAdder = Number(localStorage.getItem('autoClickAdderValue'));
-    clickBoosters[0].children[0].innerHTML = localStorage.getItem('autoClickBoosterAmount');
-    clickBoosters[1].children[0].innerHTML = localStorage.getItem('multiplyClickBoosterAmount');
-    clickBoosters[1].children[1].innerHTML = localStorage.getItem('multiplyClickBoosterText');
+    clickBoosters[0].children[0].innerText = localStorage.getItem('autoClickBoosterAmount');
+    clickBoosters[1].children[0].innerText = localStorage.getItem('multiplyClickBoosterAmount');
+    clickBoosters[1].children[1].innerText = localStorage.getItem('multiplyClickBoosterText');
 }
-} */
+}
 btn.addEventListener('click', () => {
     whenBtnClicked();
 });
 clickBoosters.forEach((el, index) => {
     el.addEventListener('click', () => {
         if (index === 0) {
-            if (Number(clickCounter.innerHTML) >= Number(el.children[0].innerHTML)) {
-                let amount = Number(el.children[0].innerHTML);
+            if (Number(clickCounter.innerText) >= Number(el.children[0].innerText)) {
+                let amount = Number(el.children[0].innerText);
                 clickCounterTakeAmount(amount);
                 changeAutoClick();
+                el.children[0].innerText = el.children[0].innerText*2
             }
         }
         if (index === 1) {
-            if (Number(clickCounter.innerHTML) >= Number(el.children[0].innerHTML)) {
-                let amount = Number(el.children[0].innerHTML);
+            if (Number(clickCounter.innerText) >= Number(el.children[0].innerText)) {
+                let amount = Number(el.children[0].innerText);
                 clickCounterTakeAmount(amount);
                 changeMultiply();
+                el.children[0].innerText = el.children[0].innerText*2
+                el.children[1].innerText = `Click multiplier x${multiply+1} `
             }
         }
     })
 });
-/*
+
 saveBtn.addEventListener('click', () => {
     save();
 });
 loadBtn.addEventListener('click', () => {
     load();
 })
-*/
